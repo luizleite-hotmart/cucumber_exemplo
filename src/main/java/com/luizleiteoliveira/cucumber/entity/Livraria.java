5bc0bfa23ff692acf93cadf211985108a9548384
luizleite <luiz.leite@hotmart.com.br>
2018-04-07 09:12:07 -0300

    implementação de livraria

--- src/main/java/com/luizleiteoliveira/cucumber/entity/Livraria.java
+++ src/main/java/com/luizleiteoliveira/cucumber/entity/Livraria.java
@@ -1,4 +1,29 @@
 package com.luizleiteoliveira.cucumber.entity;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
 public class Livraria {
+
+    private final List<Livro> loja = new ArrayList<>();
+
+    public void addLivro(final Livro livro) {
+        loja.add(livro);
+    }
+
+    public List<Livro> findLivros(final Date from, final Date to) {
+        Calendar end = Calendar.getInstance();
+        end.setTime(to);
+        end.roll(Calendar.YEAR, 1);
+        return loja.stream().filter(livro -> {
+            return from.before(livro.getPublicacao()) && end.getTime().after(livro.getPublicacao());
+        }).sorted(Comparator.comparing(Livro::getPublicacao).reversed()).collect(Collectors.toList());
+    }
 }
