57d08a41aa84a09f96f356b4b43667c4dff97362
luizleite <luiz.leite@hotmart.com.br>
2018-04-07 09:32:05 -0300

    change_test_app

--- src/test/java/com/luizleiteoliveira/cucumber/CucumberApplicationTests.java
+++ src/test/java/com/luizleiteoliveira/cucumber/CucumberApplicationTests.java
@@ -1,16 +1,14 @@
 package com.luizleiteoliveira.cucumber;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/hotmart/cucumber/resources")
 public class CucumberApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
 
 }
