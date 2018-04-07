128186aebffe2efca829d0cbebb7c65c9b81ff75
luizleite <luiz.leite@hotmart.com.br>
2018-04-07 09:06:42 -0300

    implementação de livro

--- src/main/java/com/luizleiteoliveira/cucumber/entity/Livro.java
+++ src/main/java/com/luizleiteoliveira/cucumber/entity/Livro.java
@@ -1,4 +1,19 @@
 package com.luizleiteoliveira.cucumber.entity;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Livro {
+
+    private String titulo;
+    private String autor;
+    private Date publicacao;
+
+    public Livro(String titulo, String autor, Date publicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.publicacao = publicacao;
+    }
 }
