6ca6a4a6c7838c08cb1daf07e6e4823075a3912c
luizleite <luiz.leite@hotmart.com.br>
2018-04-07 09:16:25 -0300

    change_lombok

--- src/main/java/com/luizleiteoliveira/cucumber/entity/Livro.java
+++ src/main/java/com/luizleiteoliveira/cucumber/entity/Livro.java
@@ -1,14 +1,25 @@
 package com.luizleiteoliveira.cucumber.entity;
 
-import lombok.Data;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Date;
 
-@Data
+@ToString
+@EqualsAndHashCode
 public class Livro {
 
+    @Getter
+    @Setter
     private String titulo;
+    @Getter
+    @Setter
     private String autor;
+    @Getter
+    @Setter
     private Date publicacao;
 
     public Livro(String titulo, String autor, Date publicacao) {
